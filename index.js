e939a4e712526a3e6bdbfb5564a615a362d89ff9
Mark Daniels <mark_daniels1982@hotmail.com>
2015-05-19 16:23:59 -0700

    node calc refactored

--- index.js
+++ index.js
@@ -14,16 +14,40 @@ app.get("/multiply/:x/:y", function(req, res) {
   res.send("The answer is: " + (req.params.y * req.params.x));
 })
 
-app.get("/add/:x/:y", function(req, res) {
-  res.send("The answer is: " + (parseInt(req.params.x) + parseInt(req.params.y)));
-})
+// app.get("/add/:x/:y", function(req, res) {
+//   res.send("The answer is: " + (parseInt(req.params.x) + parseInt(req.params.y)));
+// })
 
-app.get("/subtract/:x/:y", function(req, res) {
-  res.send("The answer is: " + (parseInt(req.params.x) - (req.params.y)));
-})
+// app.get("/subtract/:x/:y", function(req, res) {
+//   res.send("The answer is: " + (parseInt(req.params.x) - (req.params.y)));
+// })
 
 app.get("/divide/:x/:y", function(req, res) {
   res.send("The answer is: " + (req.params.x / req.params.y));
+})
+
+app.get("/add/*", function(req, res) {
+  var myParams = req.params[0].split("/")
+  var result = myParams.reduce(function(total, num) {return total + parseInt(num)}, 0)
+  res.send("The answer is: " + result);
+})
+
+app.get("/subtract/*", function(req, res) {
+  var myParams = req.params[0].split("/")
+  var result = myParams.reduce(function(total, num) {return total - parseInt(num)}, 0)
+  res.send("The answer is: " + result);
+})
+
+app.get("/multiply/*", function(req, res) {
+  var myParams = req.params[0].split("/")
+  var result = myParams.reduce(function(total, num) {return total * parseInt(num)}, 0)
+  res.send("The answer is: " + result);
+})
+
+app.get("/divide/*", function(req, res) {
+  var myParams = req.params[0].split("/")
+  var result = myParams.reduce(function(total, num) {return total / parseInt(num)}, 0)
+  res.send("The answer is: " + result);
 })
 
 app.listen(3000);
